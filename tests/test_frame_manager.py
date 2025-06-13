28c44433276400ec9ece75a333648e91eb4858bf
sjensen1024 <stevenmjensen1024@gmail.com>
2025-06-12 19:43:46 -0500

    added test for put_frame_over_background method

--- tests/test_frame_manager.py
+++ tests/test_frame_manager.py
@@ -18,10 +18,11 @@ class TestFrameManager(unittest.TestCase):
         self.assertFalse(self.frame_manager.get_is_saved())
 
     def test_save_changes_is_saved_attribute(self):
+        original_image_save = PIL.Image.Image.save
         PIL.Image.Image.save = MagicMock(name='mock_image_save')
         self.frame_manager.save_frame(ROOT_DIR + '\\support\\test_workspaces\\has_files_and_directories\\original_frames')
         self.assertTrue(self.frame_manager.get_is_saved())
-        PIL.Image.Image.save = PIL.Image.Image.save
+        PIL.Image.Image.save = original_image_save
 
     def test_rembg_remove_is_called_on_remove_background_from_frame(self):
         original_rembg_remove = rembg.remove
@@ -29,3 +30,14 @@ class TestFrameManager(unittest.TestCase):
         self.frame_manager.remove_background_from_frame()
         rembg.remove.assert_called()
         rembg.remove = original_rembg_remove
+
+    def test_paste_is_called_on_converted_image_on_put_frame_over_background(self):
+        original_image_paste = PIL.Image.Image.paste
+        original_image_convert = PIL.Image.Image.convert
+        PIL.Image.Image.paste = MagicMock(name='mock_image_paste')
+        PIL.Image.Image.convert = MagicMock(name='mock_image_convert')
+        self.frame_manager.put_frame_over_color_background((0,0,0))
+        PIL.Image.Image.convert.assert_called()
+        PIL.Image.Image.paste.assert_called()
+        PIL.Image.Image.paste = original_image_paste
+        PIL.Image.Image.convert = original_image_convert 
