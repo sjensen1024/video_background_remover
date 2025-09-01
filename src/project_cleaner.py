fa4b2c34b9e1ec2d4380035271bce1ef4f3d2ffd
sjensen1024 <stevenmjensen1024@gmail.com>
2025-09-01 08:23:59 -0500

    removed name mangling in ProjectCleaner and reformatted initializer params

--- src/project_cleaner.py
+++ src/project_cleaner.py
@@ -1,30 +1,35 @@
 import os
 
 class ProjectCleaner:
-    def __init__(self, original_frames_directory_name = '', transparent_frames_directory_name = '', background_color_frames_directory_name = '', result_output_file_name = ''):
+    def __init__(self, 
+                 original_frames_directory_name = '', 
+                 transparent_frames_directory_name = '', 
+                 background_color_frames_directory_name = '', 
+                 result_output_file_name = ''
+                 ):
         self.original_frames_directory_name = original_frames_directory_name
         self.transparent_frames_directory_name = transparent_frames_directory_name
         self.background_color_frames_directory_name = background_color_frames_directory_name
         self.result_output_file_name = result_output_file_name
-        self.clean_original_frames_result = self.__status_messages().get('new')
-        self.clean_transparent_frames_result = self.__status_messages().get('new')
-        self.clean_background_color_frames_result = self.__status_messages().get('new')
-        self.clean_result_output_result = self.__status_messages().get('new')
+        self.clean_original_frames_result = self._status_messages().get('new')
+        self.clean_transparent_frames_result = self._status_messages().get('new')
+        self.clean_background_color_frames_result = self._status_messages().get('new')
+        self.clean_result_output_result = self._status_messages().get('new')
 
     def clean_current_workspace(self):
         print('Start the process of cleaning the current workspace.')
-        self.__print_process_start_output_for_path(self.original_frames_directory_name)
-        self.__clean_original_frames_directory()
-        self.__print_process_result_output_for_path('clean_original_frames_result')
-        self.__print_process_start_output_for_path(self.transparent_frames_directory_name)
-        self.__clean_transparent_frames_directory()
-        self.__print_process_result_output_for_path('clean_transparent_frames_result')
-        self.__print_process_start_output_for_path(self.background_color_frames_directory_name)
-        self.__clean_background_color_frames_directory()
-        self.__print_process_result_output_for_path('clean_background_color_frames_result')
-        self.__print_process_start_output_for_path(self.result_output_file_name)
-        self.__clean_result_output()
-        self.__print_process_result_output_for_path('clean_result_output_result')
+        self._print_process_start_output_for_path(self.original_frames_directory_name)
+        self._clean_original_frames_directory()
+        self._print_process_result_output_for_path('clean_original_frames_result')
+        self._print_process_start_output_for_path(self.transparent_frames_directory_name)
+        self._clean_transparent_frames_directory()
+        self._print_process_result_output_for_path('clean_transparent_frames_result')
+        self._print_process_start_output_for_path(self.background_color_frames_directory_name)
+        self._clean_background_color_frames_directory()
+        self._print_process_result_output_for_path('clean_background_color_frames_result')
+        self._print_process_start_output_for_path(self.result_output_file_name)
+        self._clean_result_output()
+        self._print_process_result_output_for_path('clean_result_output_result')
         print('Finished the process of cleaning the current workspace.')
 
     def get_status(self):
@@ -35,13 +40,13 @@ class ProjectCleaner:
             'clean_result_output_result': self.clean_result_output_result
         }
     
-    def __print_process_start_output_for_path(self, path):
+    def _print_process_start_output_for_path(self, path):
         print('\tStarting cleaning process for ' + path)
 
-    def __print_process_result_output_for_path(self, result_key):
+    def _print_process_result_output_for_path(self, result_key):
         print('\t\tResult: ' + self.get_status().get(result_key))
     
-    def __status_messages(self):
+    def _status_messages(self):
         return {
             'new': 'Not yet run.',
             'directory_does_not_exist': 'This directory does not exist, so it cannot be cleaned.',
@@ -50,37 +55,37 @@ class ProjectCleaner:
             'success': 'Successfully cleaned.'
         }
 
-    def __clean_original_frames_directory(self):
-        self.__clean_all_pngs_in_directory(self.original_frames_directory_name)
+    def _clean_original_frames_directory(self):
+        self._clean_all_pngs_in_directory(self.original_frames_directory_name)
 
-    def __clean_transparent_frames_directory(self):
-        self.__clean_all_pngs_in_directory(self.transparent_frames_directory_name)
+    def _clean_transparent_frames_directory(self):
+        self._clean_all_pngs_in_directory(self.transparent_frames_directory_name)
 
-    def __clean_background_color_frames_directory(self):
-        self.__clean_all_pngs_in_directory(self.background_color_frames_directory_name)
+    def _clean_background_color_frames_directory(self):
+        self._clean_all_pngs_in_directory(self.background_color_frames_directory_name)
 
-    def __clean_result_output(self):
+    def _clean_result_output(self):
         if not os.path.exists(self.result_output_file_name):
-            self.clean_result_output_result = self.__status_messages().get('file_does_not_exist')
+            self.clean_result_output_result = self._status_messages().get('file_does_not_exist')
             return
         os.remove(self.result_output_file_name)
-        self.clean_result_output_result = self.__status_messages().get('success')
+        self.clean_result_output_result = self._status_messages().get('success')
 
-    def __clean_all_pngs_in_directory(self, directory):
+    def _clean_all_pngs_in_directory(self, directory):
         if not os.path.exists(directory):
-            self.__set_status_result_for_directory(directory, self.__status_messages().get('directory_does_not_exist'))
+            self._set_status_result_for_directory(directory, self._status_messages().get('directory_does_not_exist'))
             return   
 
         existing_file_names = os.listdir(directory)
         existing_png_file_names = list(filter(lambda file_name: file_name.endswith('png'), existing_file_names))
         if not existing_png_file_names:
-            self.__set_status_result_for_directory(directory, self.__status_messages().get('directory_is_empty'))
+            self._set_status_result_for_directory(directory, self._status_messages().get('directory_is_empty'))
             return
         
-        [self.__delete_file_from_directory(file_name, directory) for file_name in existing_png_file_names]
-        self.__set_status_result_for_directory(directory, self.__status_messages().get('success'))
+        [self._delete_file_from_directory(file_name, directory) for file_name in existing_png_file_names]
+        self._set_status_result_for_directory(directory, self._status_messages().get('success'))
 
-    def __set_status_result_for_directory(self, directory, result_message):
+    def _set_status_result_for_directory(self, directory, result_message):
         if directory == self.original_frames_directory_name:
             self.clean_original_frames_result = result_message
             return
@@ -89,7 +94,7 @@ class ProjectCleaner:
             return
         self.clean_background_color_frames_result = result_message
 
-    def __delete_file_from_directory(self, file_name, directory):
+    def _delete_file_from_directory(self, file_name, directory):
         file_path = os.path.join(directory, file_name)
         os.unlink(file_path)
         print('\t\t' + file_name + ' deleted.')
